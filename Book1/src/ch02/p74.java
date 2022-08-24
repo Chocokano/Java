284ba6d9c526387bf61e8729439ed5e5c5adaa6d
lovelyroaflower <lovelyroaflower@gmail.com>
2022-08-24 13:42:14 +0900

    Java 수업예제

---
+++ Book1/src/ch02/p74.java
@@ -0,0 +1,22 @@
+package ch02;
+
+public class p74 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int mathScore = 90;
+		int engScore = 70;
+		
+		int totalScore = mathScore + engScore; //총점 구하기
+		System.out.println(totalScore);
+		
+		double avgSocre = totalScore / 2.0; //평균 구하기
+		System.out.println(avgSocre);
+	
+		
+				
+
+	}
+
+}
---
+++ Book1/src/ch02/p81.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class p81 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge)? 'T' :'F';
+		
+	System.out.println(ch);
+	}
+
+}
