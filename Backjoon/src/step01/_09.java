f32d03112b99c0eaee6a2d5b94ef52e7a718594e
lovelyroaflower <lovelyroaflower@gmail.com>
2022-08-25 13:41:26 +0900

    Java 수업예제

--- Backjoon/src/step01/_09.java
+++ Backjoon/src/step01/_09.java
@@ -1,12 +1,18 @@
 package step01;
+
+import java.util.Scanner;
+
 /*
- * 날짜 : 2022/08/19
+ * 날짜 : 2022/08/25
  * 이름 : 최아영
- * 내용 : 1단계 1번 : Hello World
+ * 내용 : 1단계 9번 : 1998년생인 내가 태국에서는 2541년생?!
  */
 public class _09 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n-543); 
+
 	}
 
 }
