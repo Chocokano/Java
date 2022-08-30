97ad1a532b56890a791713bdfdc28105b76f3e45
lovelyroaflower <lovelyroaflower@gmail.com>
2022-08-31 08:54:15 +0900

    Java 문제풀이

--- Backjoon/src/step02/_03.java
+++ Backjoon/src/step02/_03.java
@@ -3,7 +3,7 @@ package step02;
 import java.util.Scanner;
 
 /*
- * 날짜 : 2022/08/30
+ * 날짜 : 2022/08/31
  * 이름 : 최아영
  * 내용 : 2단계 3번 : 윤년
  */
@@ -11,10 +11,22 @@ import java.util.Scanner;
 public class _03 {
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		
-		int year = sc.nextInt();
+
+		int y = in.nextInt();
 		
-	
+        
+		if(y%4==0) {
+			
+			if(y%400==0) System.out.println("1");
+			
+			else if(y%100==0) System.out.println("0");
+			
+			else System.out.println("1");
+		}
+		
+		else System.out.println("0");
+		
 	}
 }
