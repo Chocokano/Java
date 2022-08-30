edec3362f423479d9957119fc0da2fb0611be117
lovelyroaflower <lovelyroaflower@gmail.com>
2022-08-30 13:38:53 +0900

    Java 수업예제

--- Exam/src/sub01/Test08.java
+++ Exam/src/sub01/Test08.java
@@ -16,9 +16,9 @@ public class Test08 {
 			
 	
 			for(int j=n-1; j>i ; j--) {
-			 System.out.print("");
+			 System.out.print(" ");
 		}
-			for(int j=0;j<2 * i + 1 ; j++) {
+			for(int j=0; j<2 * i + 1 ; j++) {
 				System.out.print("*");
 				
 			}
