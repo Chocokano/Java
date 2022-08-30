7d7248b6ef1f4bfb1101b8d5e072eb8800bdae04
lovelyroaflower <lovelyroaflower@gmail.com>
2022-08-30 13:38:19 +0900

    Java 문제풀이

---
+++ Backjoon/src/step02/_02.java
@@ -0,0 +1,32 @@
+package step02;
+
+import java.util.Scanner;
+/*
+ * 날짜 : 2022/08/30
+ * 이름 : 최아영
+ * 내용 : 2단계 2번 : 시험성적
+ */
+
+public class _02 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int score = sc.nextInt();
+		
+		sc.close();
+		
+		 if( score >= 90 ) {
+	        	System.out.println( 'A' );
+	        } else if( score >= 80 ) {
+	        	System.out.println( 'B' );
+	        } else if( score >= 70 ) {
+	        	System.out.println( 'C' );
+	        } else if( score >= 60 ) {
+	        	System.out.println( 'D' );
+	        } else {
+	        	System.out.println( 'F' );
+	        }
+		
+	}
+}
---
+++ Backjoon/src/step02/_03.java
@@ -0,0 +1,20 @@
+package step02;
+
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/30
+ * 이름 : 최아영
+ * 내용 : 2단계 3번 : 윤년
+ */
+
+public class _03 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		
+	
+	}
+}
